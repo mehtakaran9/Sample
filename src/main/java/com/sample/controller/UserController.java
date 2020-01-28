d0f48bc27b1600b3285a431916c63272982af2bf
Karan Mehta <karan.mehta@gdn-commerce.com>
2020-01-28 14:56:57 +0530

    Sample Mongo Project

---
+++ src/main/java/com/sample/controller/UserController.java
@@ -0,0 +1,103 @@
+package com.sample.controller;
+
+import java.util.List;
+
+import com.sample.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import com.sample.dal.UserDAL;
+import com.sample.dal.UserRepository;
+
+@RestController
+@RequestMapping(value = "/")
+public class UserController {
+
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+	private final UserRepository userRepository;
+
+	private final UserDAL userDAL;
+
+	public UserController(UserRepository userRepository, UserDAL userDAL) {
+		this.userRepository = userRepository;
+		this.userDAL = userDAL;
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public User addNewUsers(@RequestBody User user) {
+		LOG.info("Saving user.");
+		return userRepository.save(user);
+	}
+
+	@RequestMapping(value = "/update/{userId}", method = RequestMethod.POST)
+	public String updateCount(@PathVariable String userId) {
+
+		return userDAL.updateCount(userId) + " ";
+
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<User> getAllUsers() {
+		LOG.info("Getting all users.");
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable String userId) {
+		LOG.info("Getting user with ID: {}.", userId);
+		return userRepository.findOne(userId);
+	}
+
+	// @RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
+	// public Object getAllUserSettings(@PathVariable String userId) {
+	// User user = userRepository.findOne(userId);
+	// if (user != null) {
+	// return user.getUserSettings();
+	// } else {
+	// return "User not found.";
+	// }
+	// }
+
+	@RequestMapping(value = "/settings/{userId}", method = RequestMethod.GET)
+	public Object getAllUserSettings(@PathVariable String userId) {
+		User user = userRepository.findOne(userId);
+		if (user != null) {
+			return userDAL.getAllUserSettings(userId);
+		} else {
+			return "User not found.";
+		}
+	}
+
+	// @RequestMapping(value = "/settings/{userId}/{key}", method =
+	// RequestMethod.GET)
+	// public String getUserSetting(@PathVariable String userId, @PathVariable
+	// String key) {
+	// //User user = userRepository.findOne(userId);
+	// String setting = userDAL.getUserSetting(userId, key);
+	// LOG.info("Setting = "+setting);
+	// if (setting != null) {
+	// return setting;
+	// } else {
+	// return "Setting not found.";
+	// }
+	// }
+
+	@RequestMapping(value = "/settings/{userId}/{key}", method = RequestMethod.GET)
+	public String getUserSetting(@PathVariable String userId, @PathVariable String key) {
+		return userDAL.getUserSetting(userId, key);
+	}
+
+	@RequestMapping(value = "/settings/{userId}/{key}/{value}", method = RequestMethod.GET)
+	public String addUserSetting(@PathVariable String userId, @PathVariable String key, @PathVariable String value) {
+		User user = userRepository.findOne(userId);
+		if (user != null) {
+			user.getUserSettings().put(key, value);
+			userRepository.save(user);
+			return "Key added";
+		} else {
+			return "User not found.";
+		}
+	}
+}
---
+++ src/main/java/com/sample/dal/UserDALImpl.java
@@ -0,0 +1,86 @@
+package com.sample.dal;
+
+import java.util.List;
+
+import com.mongodb.WriteResult;
+import com.sample.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDALImpl implements UserDAL {
+	private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Override
+	public List<User> getAllUsers() {
+		return mongoTemplate.findAll(User.class);
+	}
+
+	@Override
+	public User getUserById(String userId) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("userId").is(userId));
+		return mongoTemplate.findOne(query, User.class);
+	}
+
+	@Override
+	public User addNewUser(User user) {
+		mongoTemplate.save(user);
+		// Now, user object will contain the ID as well
+		return user;
+	}
+
+	@Override
+	public Object getAllUserSettings(String userId) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("userId").is(userId));
+		User user = mongoTemplate.findOne(query, User.class);
+		return user != null ? user.getUserSettings() : "User not found.";
+	}
+
+	@Override
+	public String getUserSetting(String userId, String key) {
+		Query query = new Query();
+		query.fields().include("userSettings");
+		query.addCriteria(Criteria.where("userId").is(userId).andOperator(Criteria.where("userSettings." + key).exists(true)));
+		User user = mongoTemplate.findOne(query, User.class);
+		return user != null ? user.getUserSettings().get(key) : "Not found.";
+	}
+
+	@Override
+	public String addUserSetting(String userId, String key, String value) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("userId").is(userId));
+		User user = mongoTemplate.findOne(query, User.class);
+		if (user != null) {
+			user.getUserSettings().put(key, value);
+			mongoTemplate.save(user);
+			return "Key added.";
+		} else {
+			return "User not found.";
+		}
+	}
+
+	@Override public boolean updateCount(String name) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("name").is(name));
+		query.addCriteria(Criteria.where("count").gt(0));
+		Update update = new Update();
+		update.inc("count", -1);
+		WriteResult writeResult = mongoTemplate.updateFirst(query, update, User.class);
+		if (writeResult.isUpdateOfExisting()) {
+			return true;
+		} else
+			return false;
+	}
+}
