944c979c658f263e41474f53748013989e943101
Karan Mehta <karan.mehta@gdn-commerce.com>
2020-01-30 11:08:13 +0530

    Swagger Configuration added

--- src/main/java/com/sample/controller/UserController.java
+++ src/main/java/com/sample/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
